daa7aeb3043eeecb1874d684b6a14295c815adf4
Nils Deppe <nils@nilsdeppe.com>
2025-05-08 11:17:49 -0400

    Add learning initial conditions and params

--- src/PredatorPreyOptimize.jl
+++ src/PredatorPreyOptimize.jl
@@ -70,7 +70,7 @@ scatter!(
 # is working in Greek space, we will be passed the parameters, then we solve the
 # ODE over time, and then we compute the difference between the data and the
 # solution using some suitable norm (e.g. L2).
-function loss(new_parameters)
+function loss_learn_parameters(new_parameters)
   # Create the new ODEProblem with the current parameters
   # new_problem = remake(problem, p = new_parameters)
   new_problem = ODEProblem(pred_prey!, u_0, t_span, new_parameters)
@@ -84,7 +84,7 @@ function loss(new_parameters)
   return loss
 end
 
-function callback(state, l)
+function callback_learn_parameters(state, l)
   display(l)
   newprob = remake(problem, p = state.u)
   sol = solve(newprob, saveat = 0.1)
@@ -98,26 +98,147 @@ function callback(state, l)
   return false
 end
 
-# Set up the optimization problem with our loss function and initial guess
-adtype = AutoForwardDiff()
-p_guess = [1.5, 1.0, 2.71, 1.5]
-optf = OptimizationFunction((x, _) -> loss(x), adtype)
-optprob = OptimizationProblem(
-  optf,
-  p_guess,
-  lb = [0.5, 0.5, 0.5, 0.5],
-  ub = [5.5, 5.5, 5.5, 5.5],
-)
+function learn_parameters()
+  # This function learns the α,β,γ,δ parameters of the Lotka-Volterra equation.
+  #
+  # Set up the optimization problem with our loss function and initial guess
+  adtype = AutoForwardDiff()
+  p_guess = [1.5, 1.0, 2.71, 1.5]
+  optf = OptimizationFunction((x, _) -> loss_learn_parameters(x), adtype)
+  optprob = OptimizationProblem(
+    optf,
+    p_guess,
+    lb = [0.5, 0.5, 0.5, 0.5],
+    ub = [5.5, 5.5, 5.5, 5.5],
+  )
 
-# Optimize the ODE parameters for best fit to our data
-pfinal = solve(
-  optprob,
-  Optim.BFGS(),
-  # callback = callback,
-  abstol = 1.0e-60,
-  reltol = 1.0e-13,
-  maxiters = 200,
-)
-α, β, γ, δ = round.(pfinal, digits = 1)
-print("Final params:   ", pfinal)
-print("Correct params: ", p_true)
+  # Optimize the ODE parameters for best fit to our data
+  pfinal = solve(
+    optprob,
+    Optim.BFGS(),
+    # callback = callback_learn_parameters,
+    abstol = 1.0e-60,
+    reltol = 1.0e-13,
+    maxiters = 200,
+  )
+  α, β, γ, δ = round.(pfinal, digits = 1)
+  print("Final params:   ", pfinal)
+  print("Correct params: ", p_true)
+  return nothing
+end
+
+function learn_initial_conditions(true_parameters)
+  # Now that we have set up a condition for whose parameters we want to
+  # optimize, we need to set up the loss function.
+  #
+  # The loss function is called by the optimizer at each step. Since the
+  # optimizer is working in Greek space, we will be passed the parameters, then
+  # we solve the ODE over time, and then we compute the difference between the
+  # data and the solution using some suitable norm (e.g. L2).
+  function loss_initial_conditions(new_initial_conditions)
+    # Create the new ODEProblem with the current parameters
+    # new_problem = remake(problem, p = new_parameters)
+    new_problem =
+      ODEProblem(pred_prey!, new_initial_conditions, t_span, true_parameters)
+    # Solve it, dumping at the times we have data.
+    sol =
+      solve(new_problem, Vern9(), reltol = 1e-13, abstol = 1e-30, saveat = 1)
+    # For the lass, just sum up the squares of the differences and take the
+    # square root. The square root is important since otherwise we are counting
+    # the digits/accuracy of the square of the problem.
+    loss =
+      sqrt(sum(abs2, collect(Compat.Iterators.flatten(sol.u .- initial_sol.u))))
+    return loss
+  end
+
+  u_0_guess = [0.5, 1.5]
+  # This function learns the initial conditions of the Lotka-Volterra equation.
+  #
+  # Set up the optimization problem with our loss function and initial guess
+  adtype = AutoForwardDiff()
+  optf = OptimizationFunction((x, _) -> loss_initial_conditions(x), adtype)
+  optprob = OptimizationProblem(
+    optf,
+    u_0_guess,
+    lb = [0.1, 0.1],
+    ub = [5.5, 5.5],
+  )
+
+  # Optimize the ODE parameters for best fit to our data
+  pfinal = solve(
+    optprob,
+    Optim.BFGS(),
+    # callback = callback,
+    abstol = 1.0e-60,
+    reltol = 1.0e-13,
+    maxiters = 200,
+  )
+  print("Final initial conditions:   ", pfinal)
+  print("Correct initial conditions: ", u_0)
+  return nothing
+end
+
+# Finally, we try to learn both the initial conditions _and_ the parameters.
+# This case is a lot more challenging because of a lot of local minima in the
+# parameter space. If you choose initial guess "close enough", then the solution
+# converges. I haven't done a detailed study of how close "close enough" is, but
+# the values right now seem to work, while if you use guesses for the initial
+# conditions of [0.5, 1.5] then you end up stuck in a local minimum.
+function learn_parameters_and_initial_conditions()
+  # Now that we have set up a condition for whose parameters we want to
+  # optimize, we need to set up the loss function.
+  #
+  # The loss function is called by the optimizer at each step. Since the
+  # optimizer is working in Greek space, we will be passed the parameters, then
+  # we solve the ODE over time, and then we compute the difference between the
+  # data and the solution using some suitable norm (e.g. L2).
+  function loss_parameters_and_initial_conditions(new_initial_conditions_and_parameters)
+    # Create the new ODEProblem with the current parameters
+    # new_problem = remake(problem, p = new_parameters)
+    new_problem =
+      ODEProblem(pred_prey!, new_initial_conditions_and_parameters[1:2], t_span,
+          new_initial_conditions_and_parameters[3:end])
+    # Solve it, dumping at the times we have data.
+    sol =
+      solve(new_problem, Vern9(), reltol = 1e-13, abstol = 1e-30, saveat = 1)
+    # For the lass, just sum up the squares of the differences and take the
+    # square root. The square root is important since otherwise we are counting
+    # the digits/accuracy of the square of the problem.
+    loss =
+      sqrt(sum(abs2, collect(Compat.Iterators.flatten(sol.u .- initial_sol.u))))
+    return loss
+  end
+
+  initial_guess = [0.8, 1.1, 1.5, 1.0, 2.71, 1.5]
+  # This function learns the initial conditions of the Lotka-Volterra equation.
+  #
+  # Set up the optimization problem with our loss function and initial guess
+  adtype = AutoForwardDiff()
+  optf = OptimizationFunction((x, _) -> loss_parameters_and_initial_conditions(x), adtype)
+  optprob = OptimizationProblem(
+    optf,
+    initial_guess,
+    lb = [0.1, 0.1, 0.5, 0.5, 0.5, 0.5],
+    ub = [5.5, 5.5, 5.5, 5.5, 5.5, 5.5],
+  )
+
+  # Optimize the ODE parameters for best fit to our data
+  pfinal = solve(
+    optprob,
+    Optim.BFGS(),
+    # callback = callback,
+    abstol = 1.0e-60,
+    reltol = 1.0e-13,
+    maxiters = 200,
+  )
+  print("Final loss function: ",
+        loss_parameters_and_initial_conditions(pfinal.u), "\n")
+  print("Final initial conditions:   ", pfinal.u[1:2], pfinal.u[3:end],
+      "\n")
+  print("Correct initial conditions: ", u_0, p_true)
+  return nothing
+end
+
+learn_parameters()
+learn_initial_conditions(p_true)
+learn_parameters_and_initial_conditions()
